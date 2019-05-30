700427dd75cd4e11ce59f23ddc19d26c09b04320
EmilbekovaGulzada <emilbekovagulzada.kg@gmail.com>
2019-05-30 19:31:18 +0600

    add some logic

--- gigant/url.py
+++ gigant/url.py
@@ -1,16 +1,18 @@
-from django.urls import path
-from gigant.views import base_view, category_view, car_view, show_category
+from django.urls import path, re_path
+from gigant.views import base_view, category_view, car_view, show_category, product_view, CategoryView
 from gigant import views as core_views
 
 
 
 urlpatterns = [
-    path('car/<slug:car_slug>/category/<slug:category_slug>/', show_category, name='category'),
-    path('car/<slug:car_slug>/', car_view, name='car_detail'),
+    # path('category/<str:slug>/', CategoryView.as_view(), name="product-detail"),
+    # path('car/<slug:slug>/category/<slug:category_slug>/', show_category, name='category'),
+    re_path(r'^car/(?P<slug>\w+)(?:/category/(?P<category_slug>\w+))?/$', CategoryView.as_view(), name='category'),
+    path('product/<slug:t_slproducug>/', product_view, name='product'),
     path('activate/', core_views.activate, name='activate'),
     path('',base_view, name='base'),
 
+
+
+
 ]
-
-
-
